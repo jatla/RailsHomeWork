41e52c736428b509f0eecc8e081c49e5529f6c73
JayaPrakash Reddy Atla <jatla@Sunithas-MacBook-Air.local>
2014-04-16 20:13:37 -0700

    New application to handle home work for Rails class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsHomeWork::Application.config.secret_key_base = '308f13d25a03b1bf127fc539713f29771ba6a95f2ff11987c70e90c8004b5fc869c6dad890ec61a8292539cf360438982cf295bc2fe9a5b7397873720db97b08'
